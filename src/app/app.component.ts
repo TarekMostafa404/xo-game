2ebe241b20bde9fe2ed6b18befb3213abfff39e1
Tarek Mostafa <80650764+TarekMostafa404@users.noreply.github.com>
2023-04-17 17:02:55 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'xo-game';
+
+  storeValue = '';
+  boxesValue: any[] = ['', '', '', '', '', '', '', '', ''];
+
+  printValue(index:number) {
+    alert(this.boxesValue[index]);
+  }
 }
