0825e9ff6cf3e6ccff6c287e6d2c1a5e9adc94bb
Tarek Mostafa <80650764+TarekMostafa404@users.noreply.github.com>
2023-04-30 21:08:25 +0300

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDIwyLDZ9zKabnJwDQv_KPAUOcsA8vl68E',
+    authDomain: 'x-o-game-aa9de.firebaseapp.com',
+    projectId: 'x-o-game-aa9de',
+    storageBucket: 'x-o-game-aa9de.appspot.com',
+    messagingSenderId: '270845026300',
+    appId: '1:270845026300:web:f3b6c0c8c73781e4591c79',
+  },
+};
